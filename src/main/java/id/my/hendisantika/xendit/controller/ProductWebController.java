01db4a4a67f59f9dc056fb105fb56ccb338c3e50
hendisantika <hendisantika@yahoo.co.id>
2025-10-11 06:24:57 +0700

    feat: Add listProducts in ProductWebController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/xendit/controller/ProductWebController.java
+++ src/main/java/id/my/hendisantika/xendit/controller/ProductWebController.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.xendit.controller;
 
+import id.my.hendisantika.xendit.entity.Product;
 import id.my.hendisantika.xendit.service.ProductApiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +29,18 @@ public class ProductWebController {
 
     private final ProductApiService productApiService;
 
+    @GetMapping
+    public String listProducts(
+            @RequestParam(required = false) String search,
+            Model model) {
+        List<Product> products;
+        if (search != null && !search.isEmpty()) {
+            products = productApiService.searchProducts(search);
+            model.addAttribute("search", search);
+        } else {
+            products = productApiService.getAllProducts();
+        }
+        model.addAttribute("products", products);
+        return "products/list";
+    }
 }
