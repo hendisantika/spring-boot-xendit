95c8c2e67f32090e9cac5ab9d3a0491a5bd8ba3b
hendisantika <hendisantika@yahoo.co.id>
2025-10-11 05:29:27 +0700

    feat: Add required libs🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/xendit/SpringBootXenditApplicationTests.java
+++ src/test/java/id/my/hendisantika/xendit/SpringBootXenditApplicationTests.java
@@ -3,6 +3,17 @@ package id.my.hendisantika.xendit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-xendit
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/10/25
+ * Time: 10.50
+ * To change this template use File | Settings | File Templates.
+ */
 @SpringBootTest
 class SpringBootXenditApplicationTests {
 
