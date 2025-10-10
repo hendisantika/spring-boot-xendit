5ab52cb7185b08d526c8b12f86246e7ef1132226
hendisantika <hendisantika@yahoo.co.id>
2025-10-11 05:55:21 +0700

    feat: Add XenditConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/xendit/config/XenditConfig.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.xendit.config;
+
+import com.xendit.Xendit;
+import jakarta.annotation.PostConstruct;
+import lombok.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-xendit
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/10/25
+ * Time: 05.52
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class XenditConfig {
+
+    @Value("${xendit.api.key}")
+    private String xenditApiKey;
+
+    @PostConstruct
+    public void init() {
+        Xendit.apiKey = xenditApiKey;
+    }
+}
