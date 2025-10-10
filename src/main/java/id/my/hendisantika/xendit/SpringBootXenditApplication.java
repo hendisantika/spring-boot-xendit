60bbf8bb61e3f34c62c4325eec7357af45903d96
hendisantika <hendisantika@yahoo.co.id>
2025-10-11 05:00:02 +0700

    feat: init project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/xendit/SpringBootXenditApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.xendit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootXenditApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootXenditApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/xendit/SpringBootXenditApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.xendit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootXenditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
