5e9d6af79b22ff637bbaa89a65a45749ea548e42
hendisantika <hendisantika@yahoo.co.id>
2025-10-11 06:14:17 +0700

    feat: Add createInvoice in PaymentApiService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/xendit/service/PaymentApiService.java
+++ src/main/java/id/my/hendisantika/xendit/service/PaymentApiService.java
@@ -1,8 +1,13 @@
 package id.my.hendisantika.xendit.service;
 
+import id.my.hendisantika.xendit.dto.ApiResponse;
+import id.my.hendisantika.xendit.dto.PaymentRequestDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +25,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class PaymentApiService {
 
     private final WebClient webClient;
+
+    public Map<String, Object> createInvoice(PaymentRequestDTO paymentRequest) {
+        ApiResponse<Map<String, Object>> response = webClient.post()
+                .uri("/api/payments/create-invoice")
+                .bodyValue(paymentRequest)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<ApiResponse<Map<String, Object>>>() {
+                })
+                .block();
+        return response != null ? response.getData() : null;
+    }
 }
