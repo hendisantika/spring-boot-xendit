6d9a4eb8cc6008e6b3c631ae311b65e1d01bcb3c
hendisantika <hendisantika@yahoo.co.id>
2025-10-11 05:30:16 +0700

    feat: Add required configs🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/xendit/SpringBootXenditApplication.java
+++ src/main/java/id/my/hendisantika/xendit/SpringBootXenditApplication.java
@@ -3,6 +3,17 @@ package id.my.hendisantika.xendit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-xendit
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/10/25
+ * Time: 10.50
+ * To change this template use File | Settings | File Templates.
+ */
 @SpringBootApplication
 public class SpringBootXenditApplication {
 
