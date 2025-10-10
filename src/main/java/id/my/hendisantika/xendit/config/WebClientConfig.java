cf5faaac28170bfd30c2bd8235377c945cf6afc2
hendisantika <hendisantika@yahoo.co.id>
2025-10-11 05:52:37 +0700

    feat: Add WebClientConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/xendit/config/WebClientConfig.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.xendit.config;
+
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-xendit
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/10/25
+ * Time: 05.51
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class WebClientConfig {
+
+    @Value("${api.base.url:http://localhost:8080}")
+    private String apiBaseUrl;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(apiBaseUrl)
+                .build();
+    }
+}
