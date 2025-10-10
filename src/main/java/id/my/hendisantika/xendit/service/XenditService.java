f6bf30ac6d4f4767dccbef49f7a5577057ae0c4b
hendisantika <hendisantika@yahoo.co.id>
2025-10-11 06:09:51 +0700

    feat: Add XenditService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/xendit/service/XenditService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.xendit.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-xendit
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/10/25
+ * Time: 06.09
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class XenditService {
+
+    private final ProductService productService;
+
+}
