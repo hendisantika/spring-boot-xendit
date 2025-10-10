f63eae0f232f3bbe97afc073f7df502b3f2c96ac
hendisantika <hendisantika@yahoo.co.id>
2025-10-11 06:12:23 +0700

    feat: Add getAllProducts in ProductApiService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/xendit/service/ProductApiService.java
+++ src/main/java/id/my/hendisantika/xendit/service/ProductApiService.java
@@ -1,8 +1,13 @@
 package id.my.hendisantika.xendit.service;
 
+import id.my.hendisantika.xendit.dto.ApiResponse;
+import id.my.hendisantika.xendit.entity.Product;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +25,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class ProductApiService {
 
     private final WebClient webClient;
+
+    public List<Product> getAllProducts() {
+        ApiResponse<List<Product>> response = webClient.get()
+                .uri("/api/products")
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<ApiResponse<List<Product>>>() {
+                })
+                .block();
+        return response != null ? response.getData() : List.of();
+    }
 }
