b5743d7332e048ed5bfc494d4b28bb04c10811c6
hendisantika <hendisantika@yahoo.co.id>
2025-10-11 06:17:09 +0700

    feat: Add getAllProducts in ProductController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/xendit/controller/ProductController.java
+++ src/main/java/id/my/hendisantika/xendit/controller/ProductController.java
@@ -1,9 +1,16 @@
 package id.my.hendisantika.xendit.controller;
 
+import id.my.hendisantika.xendit.entity.Product;
 import id.my.hendisantika.xendit.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +30,12 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", true);
+        response.put("data", products);
+        return ResponseEntity.ok(response);
+    }
 }
