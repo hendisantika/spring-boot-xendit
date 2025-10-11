e530a603f9bb7f73c16de04f21f021fe320ae104
hendisantika <hendisantika@yahoo.co.id>
2025-10-11 07:26:28 +0700

    feat: Add OrderItem Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/xendit/entity/OrderItem.java
@@ -0,0 +1,59 @@
+package id.my.hendisantika.xendit.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-xendit
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/10/25
+ * Time: 10.50
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "order_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "product_price", nullable = false, precision = 19, scale = 2)
+    private BigDecimal productPrice;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "subtotal", nullable = false, precision = 19, scale = 2)
+    private BigDecimal subtotal;
+}
